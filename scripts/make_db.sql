0a5e47dcd0d9d03ad522cedce152b6d99c4fac44
Miha Drole <miha.drole@gmail.com>
2018-01-24 10:56:18 +0100

    Database adaptations

--- scripts/make_db.sql
+++ scripts/make_db.sql
@@ -1,31 +1,44 @@
-create schema vrt;
+create schema vrt
+default character set utf8
+default collate utf8_general_ci;
 
+-- TABLES
 create table vrt.vegetable_type (
-       id int,
-       type_name varchar2(256 char)
+       id int auto_increment primary key,
+       type_name varchar(256)
 );
 
 create table vrt.vegetable (
-       id int,
+       id int auto_increment primary key,
        type_id int,
-       vegetable_name varchar2(256 char)
+       vegetable_name varchar(256),
+       foreign key (type_id)
+       	       references vrt.vegetable_type(id)
 );
 
 create table vrt.herb (
-       id int,
-       herb_name varchar2(256 char)
+       id int auto_increment primary key,
+       herb_name varchar(256)
 );
 
-create table vrt.vegatable_relation (
-       id int,
+create table vrt.vegetable_relation (
+       id int auto_increment primary key,
        vegetable_id_1 int,
        vegetable_id_2 int,
-       effect int
+       effect int,
+       foreign key fk_veg_rel_veg1(vegetable_id_1)
+       	       references vrt.vegetable(id),
+       foreign key fk_veg_rel_veg2(vegetable_id_2)
+       	       references vrt.vegetable(id)       
 );
 
 create table vrt.vegetable_herb_relation (
-       id int,
+       id int auto_increment primary key,
        vegetable_id int,
        herb_id int,
-       effect int	
+       effect int,
+       foreign key (vegetable_id)
+       	       references vrt.vegetable(id),
+       foreign key (herb_id)
+       	       references vrt.herb(id)       
 );
