81c0c1239b5ff8b341ee066b108fed94c2269141
Miha Drole <miha.drole@gmail.com>
2018-02-17 11:05:49 +0100

    Changed client side to CDN

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var fileupload = require('express-fileupload');
 var dm = require('./backend/dataManipulation');
-
-var da = require('./dataAdmin');
+//var da = require('./dataAdmin');
 
 var app = express();
 
 app.use(fileupload());
-app.use(express.static('public'))
-
-// static places
-app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
-app.use('/jquery', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
-app.use('/jquery-ui/', express.static(path.join(__dirname, 'node_modules', 'jquery-ui-dist')));
-app.use('/popper/', express.static(path.join(__dirname, 'node_modules', 'popper.js', 'dist')));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main', helpers: {
